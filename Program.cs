44210e392d3d4b88f18bec7a02e6c2816c82a9a4
Kris <isaevachristin@gmail.com>
2024-02-21 17:59:15 +0300

    задача 3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿//Задачи на разных ветках.
 
+
+//Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+using System.Runtime.ExceptionServices;
+
+Console.Write("Введите любое целое число от 0 до 99: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number/100 <=0)
+{
+    int first_number = number/10;
+    int second_number = number%10;
+    if(first_number>second_number)
+    {Console.Write(first_number);}
+    else
+    {Console.Write(second_number);}
+
+}
+else
+{Console.Write("число не входит в требуемый отрезок от 10 до 99");}
