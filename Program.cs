44a1f97c3ecf0bb2ff93946cc5f2768f8c12e7ee
Kris <isaevachristin@gmail.com>
2024-02-21 18:06:04 +0300

    задача 4

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
 ﻿//Задачи на разных ветках.
 
+
+//Напишите программу, которая на вход принимает натуральное число N, 
+//а на выходе показывает его цифры через запятую.
+
+Console.Write("Введите целое число: ");
+double number = Convert.ToInt32(Console.ReadLine());
+double len = (number.ToString()).Length;
+
+double i = 1;
+double c = 10;
+double a = Math.Pow(c, len - 1);
+while (i < len )
+{
+    int result = (int)(number / a);
+    number = (int)(number % a);
+    Console.Write(result + ",");
+    a = a / 10;
+    i++;
+  
+}
+
+Console.Write(number);
